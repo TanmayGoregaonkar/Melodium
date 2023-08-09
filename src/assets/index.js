cc77aca960eb2bd843e6ace142672fc6006396cf
TanmayGoregaonkar <93464019+TanmayGoregaonkar@users.noreply.github.com>
2023-08-09 17:56:30 +0530

    initial

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import loader from './loader.svg';
+import logo from './logo.svg';
+import melo from './melo.svg';
+import melori from './melori.svg';
+export {
+  logo,
+  loader,
+  melo,
+  melori
+};
